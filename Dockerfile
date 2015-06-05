440e0d7e42012e9eb5198bee89c586af1fa40513
jbergknoff <jonathan@bergknoff.com>
2015-06-04 19:35:57 -0500

    Add client/dump/restore functionality

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM quay.io/aptible/alpine
 
 ENV DATA_DIRECTORY /var/db
 
-RUN apk-install redis=2.8.17-r0
+# rdbtools is used for importing an RDB dump remotely.
+RUN apk-install redis=2.8.17-r0 py-pip=1.5.6-r2 && pip install rdbtools
 ADD templates/redis.conf /etc/redis.conf
 
 # Integration tests
@@ -14,8 +15,8 @@ EXPOSE 6379
 
 ENV CONFIG_DIRECTORY /etc/redis
 VOLUME ["$CONFIG_DIRECTORY"]
+EXPOSE 6379
 
 ADD run-database.sh /usr/bin/
+ADD utilities.sh /usr/bin/
 ENTRYPOINT ["run-database.sh"]
-
-EXPOSE 6379
--- run-database.sh
+++ run-database.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
+. /usr/bin/utilities.sh
 ARGUMENT_FILE="$CONFIG_DIRECTORY"/arguments
+DUMP_FILENAME="/tmp/dump.rdb"
+
+start_server()
+{
+  touch $ARGUMENT_FILE # don't crash and burn if initialize wasn't called.
+  redis-server /etc/redis.conf --dir "$DATA_DIRECTORY" $(cat "$ARGUMENT_FILE")
+}
+
 if [[ "$1" == "--initialize" ]]; then
   echo "--requirepass "$PASSPHRASE"" > "$ARGUMENT_FILE"
-  exit
-fi
 
-touch $ARGUMENT_FILE # don't crash and burn if initialize wasn't called.
-redis-server /etc/redis.conf --dir "$DATA_DIRECTORY" $(cat "$ARGUMENT_FILE")
+elif [[ "$1" == "--client" ]]; then
+  [ -z "$2" ] && echo "docker run -it aptible/redis --client redis://..." && exit
+  parse_url "$2"
+  redis-cli -h "$host" -p "${port:-6379}" -a "$password"
+
+elif [[ "$1" == "--dump" ]]; then
+  [ -z "$2" ] && echo "docker run -i aptible/redis --dump redis://... > dump.rdb" && exit
+  parse_url "$2"
+  redis-cli -h "$host" -p "${port:-6379}" -a "$password" --rdb "$DUMP_FILENAME"
+  cat "$DUMP_FILENAME"
+
+elif [[ "$1" == "--restore" ]]; then
+  [ -z "$2" ] && echo "docker run -i aptible/redis --restore redis://... < dump.rdb" && exit
+  parse_url "$2"
+  cat > "$DUMP_FILENAME"
+  rdb --command protocol "$DUMP_FILENAME" | redis-cli -h "$host" -p "${port:-6379}" -a "$password" --pipe
+
+elif [[ "$1" == "--readonly" ]]; then
+  echo "This image does not support read-only mode. Starting database normally."
+  start_server
+
+else
+  start_server
+
+fi
